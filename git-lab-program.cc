e5ed49a5e28595d9145087bcedcea2b73313a6c3
Jcorey329 <jc021314@ohio.edu>
2021-01-29 15:06:58 -0500

    added git-lab-program.cc file

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: James Corey
+ *        Date: 1/29/2021
+ * Description: CS 2400, Lab 2, Git/GitHub
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
